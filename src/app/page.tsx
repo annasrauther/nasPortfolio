ee7605a8cf2f5f568c33dfa6db39f0d601683400
Annas Rauther <annasrauther95@gmail.com>
2023-06-27 20:57:43 +0530

    ui:inProgres - initial homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,73 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link";
+import style from './page.module.css'
+
+import {
+  TextEffect,
+  NeoButton,
+  SlideWrapper,
+  Slide,
+} from "@/components";
+
+const logo = "/logo.jpeg";
+const profilePic = "/profile.webp";
+const bird = "/bird-poly.png";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <>
+    <SlideWrapper>
+      <Slide backgroundColor="white">
+        <div className={style['hero-section']}>
+          <div className={style['hero-section__col']}>
+            <TextEffect
+              text="Beyond the Pixels: Crafting Experiences with Code and Design"
+              className={style['hero-section__title']}
             />
-          </a>
+            <p className={style['hero-section__title']}>
+              As a skilled full-stack developer, I am dedicated to turning
+              ideas into innovative web applications. Explore my latest
+              projects and articles, showcasing my expertise in React.js and
+              web development.
+            </p>
+            <div className={style['hero-section__callout']}>
+              <NeoButton href="/resume" text="Resume" hasBg={true} />
+              <NeoButton href="/contact" text="Contact" />
+              <Link
+                href="/contact"
+                target={"_blank"}
+                className="ml-4 text-lg font-medium underline capitalize text-dark md:text-base"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+          <div className={style['hero-section__col']}>
+            <Image
+              src={logo}
+              alt="Nas Rauther"
+              className="w-full h-auto lg:hidden md:inline-block md:w-full"
+              priority
+              width={350}
+              height={350}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Slide>
+      <Slide backgroundColor="orange">
+        <div className="grid w-full grid-cols-2 gap-4">
+          <div className="col-span-1">
+          {/* <img className="rounded-lg shadow-xl" src="https://cdn.devdojo.com/images/december2020/dashboard-011.png" alt=""/> */}
+          </div>
+          <div className="col-span-1">
+          <h3 className="mt-2 text-2xl sm:text-left md:text-4xl">Design Made Easy</h3>
+              <p className="mt-5 text-lg text-gray-700 text md:text-left">Crafting your user experience has never been easier, with our intuitive drag'n drop interface you will be creating beatiful designs in no time.</p>
+          </div>
+        </div>
+      </Slide>
+    </SlideWrapper>
+  </>
   )
 }
---
+++ src/components/index.ts
@@ -0,0 +1,27 @@
+import Navbar from './navbar/Navbar';
+import Logo from './logo/Logo';
+import TextEffect from './effects/text/TextEffect';
+import NeoButton from './button/NeoButton';
+import SlideWrapper from './scroll/SlideWrapper';
+import Slide from './scroll/Slide';
+import {
+    GithubIcon,
+    TwitterIcon,
+    LinkedInIcon,
+    MenuIcon,
+    CloseIcon,
+} from './icon/Icon';
+
+export {
+    Navbar,
+    GithubIcon,
+    TwitterIcon,
+    LinkedInIcon,
+    MenuIcon,
+    CloseIcon,
+    Logo,
+    TextEffect,
+    NeoButton,
+    SlideWrapper,
+    Slide,
+};
