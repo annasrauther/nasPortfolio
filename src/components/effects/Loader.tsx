76b0c0e9a19bc4f463053255e1550626b1689dc4
Annas Rauther <annasrauther95@gmail.com>
2023-07-29 01:15:14 +0530

    feat: implement a simple loader with svg images

--- src/components/effects/Loader.tsx
+++ src/components/effects/Loader.tsx
@@ -1,48 +1,27 @@
-"use client";
-import { useEffect, useState, useRef, useMemo } from 'react';
 import styles from './Loader.module.css';
+import Image from 'next/image';
 
 /**
- * Represents the props for the Loader component.
- */
-interface LoaderProps {
-  /**
-   * The duration in milliseconds for the loader animation.
-   */
-  duration: number;
-}
-
-/**
- * Represents the Loader component that displays a loading animation.
- * @param {LoaderProps} props - The props for the Loader component.
+ * Represents the Loader component that displays a loading svg.
  * @returns {JSX.Element} JSX.Element
  */
-const Loader: React.FC<LoaderProps> = ({ duration }) => {
-  const [counter, setCounter] = useState(0);
-  const intervalRef = useRef<number | null>(null);
-
-  // Memoize the counter value to avoid unnecessary re-computation on every render
-  const memoizedCounter = useMemo(() => counter, [counter]);
-
-  useEffect(() => {
-    intervalRef.current = window.setInterval(() => {
-      setCounter((prevCounter) => {
-        if (prevCounter === 100) {
-          clearInterval(intervalRef.current!);
-          return prevCounter;
-        }
-        return prevCounter + 1;
-      });
-    }, duration);
-
-    return () => clearInterval(intervalRef.current!);
-  }, [duration]);
-
+const Loader: React.FC = () => {
   return (
-    <div className={styles['loading-wrapper']}>
-      <div className={styles['counter']}>
-        <h1>{memoizedCounter}%</h1>
-      </div>
+    <div className={styles.loading_wrapper}>
+      <h1 className={styles.loading_text}>Loading...</h1>
+      <Image
+        className={styles.logo}
+        src="/logo.png"
+        width={75}
+        height={75}
+        alt="Logo"
+      />
+      <Image
+        src="/loader.svg"
+        width={100}
+        height={100}
+        alt="Loader"
+      />
     </div>
   );
 };
