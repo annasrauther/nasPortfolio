7798fa68141b3da0dbd0d7524e37386ae15c776a
Annas Rauther <annasrauther95@gmail.com>
2023-08-05 02:38:04 +0530

    refactor: clean up files, jsdocs for fonts, add browserlist for compatability

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'cms.annasrauther.com'
+    ]
+  },
 }
 
 module.exports = nextConfig
--- src/lib/helpers.ts
+++
@@ -1,4 +0,0 @@
-// Function to create JSX elements from the WYSIWYG content
-  export const createMarkup = (htmlContent: string) => {
-    return { __html: htmlContent };
-  };
