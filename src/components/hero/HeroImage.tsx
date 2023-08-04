8bc68b2b5cfc1e1e1d0467115689377fea4f0173
Annas Rauther <annasrauther95@gmail.com>
2023-08-04 18:05:01 +0530

    fix: improve performance of effect

--- src/components/hero/HeroImage.tsx
+++ src/components/hero/HeroImage.tsx
@@ -1,46 +1,44 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 import Image from "next/image";
 import styles from './Hero.module.css';
 
 const HeroImage: React.FC = () => {
   const heroImageRef = useRef<HTMLImageElement>(null);
+  const lastMouseYRef = useRef<number>(0); // Store mutable value in a useRef
 
-  const animateRotation = () => {
-    const mouseY = lastMouseY;
+  const animateRotation = useCallback(() => {
+    const heroImage = heroImageRef.current;
+    if (!heroImage) return;
+
+    const mouseY = lastMouseYRef.current; // Access the mutable value from useRef
     const maxRotation = 15;
 
-    const heroImage = heroImageRef.current;
-    if (heroImage) {
-      const imageRect = heroImage.getBoundingClientRect();
-      const leftRotateY = ((mouseY - imageRect.top) / imageRect.height - 0.5) * maxRotation;
-      heroImage.style.transform = `rotate(${leftRotateY}deg)`;
-    }
+    const imageRect = heroImage.getBoundingClientRect();
+    const leftRotateY = ((mouseY - imageRect.top) / imageRect.height - 0.5) * maxRotation;
+    heroImage.style.transform = `rotate(${leftRotateY}deg)`;
 
     requestAnimationFrame(animateRotation);
-  };
+  }, []); // No need to include lastMouseYRef.current in the dependency array
 
-  let lastMouseY = 0;
-  const handleMouseMove = (e: MouseEvent) => {
-    lastMouseY = e.clientY;
-  };
+  const handleMouseMove = useCallback((e: MouseEvent) => {
+    lastMouseYRef.current = e.clientY; // Update the mutable value in the useRef
+  }, []); // Empty dependency array, memoizing the function
 
   useEffect(() => {
     const isMobile = window.innerWidth <= 767;
 
     if (!isMobile) {
-      // Add mouse event listener and start animation only if not mobile
       requestAnimationFrame(animateRotation);
       document.body.addEventListener("mousemove", handleMouseMove);
     }
 
     return () => {
-      // Clean up the mouse event listener if not mobile
       if (!isMobile) {
         document.body.removeEventListener("mousemove", handleMouseMove);
       }
     };
-  }, []);
+  }, [animateRotation, handleMouseMove]);
 
   return (
     <>
